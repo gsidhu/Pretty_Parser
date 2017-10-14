c074200831334f3a06208e0ae500c5f415142508
Gurjot Sidhu <gsidhu22@gmail.com>
2017-10-14 21:37:44 +0530

    updating

--- Experimental_PrettyParser.py
+++ Experimental_PrettyParser.py
@@ -1,35 +1,46 @@
-## Pretty Parser 
-## MIT License, 2016 Gurjot S. Sidhu 
+# -*- coding: utf-8 -*-
+
+## Pretty Parser
+## MIT License, 2016 Gurjot S. Sidhu
+## Python 3
 
 from bs4 import BeautifulSoup as BS
 #import urllib.request as urr
-import webbrowser 
+import webbrowser
 import random
 import re
-import requests
+#import requests
 import os
-import datetime 
+import datetime
+#from newspaper import Article
+from goose import Goose
+import certifi
+import urllib3
+from io import open
+
+# Certificate management
+http = urllib3.PoolManager(cert_reqs='CERT_REQUIRED', ca_certs=certifi.where())
 
 home_dir = os.getcwd()
 data = {}
 feeds = {'thehindu': 'http://www.thehindu.com/news/?service=rss', 'theguardian_football': 'http://feeds.theguardian.com/theguardian/uk/sport/rss', 'wired': 'http://feeds.wired.com/wired/index', 'sci_am': 'http://rss.sciam.com/sciam/physics', 'xkcd_whatif': 'http://what-if.xkcd.com/feed.atom', 'mashable': 'http://feeds.mashable.com/Mashable', 'cnet': 'http://www.cnet.com/rss/news/', 'aljazeera': 'http://america.aljazeera.com/content/ajam/articles.rss', 'vox': 'http://www.vox.com/rss/index.xml', 'theguardian_world': 'http://www.theguardian.com/world/rss', 'empire_kop': 'http://www.empireofthekop.com/feed/', 'caravan': 'http://www.caravanmagazine.in/feed', 'new_yorker': 'http://www.newyorker.com/rss', 'newyork_times': 'http://rss.nytimes.com/services/xml/rss/nyt/InternationalHome.xml', 'brain_pickings': 'https://www.brainpickings.org/feed/', 'tribune_ludhiana': 'http://www.tribuneindia.com/rss/feed.aspx?cat_id=15', 'tribune': 'http://www.tribuneindia.com/rss/feed.aspx?cat_id=7', 'bbc': 'http://feeds.bbci.co.uk/news/rss.xml?edition=uk', 'the_wire': 'https://thewire.in/feed/'}
-feed_tags = ['sci_am', 'thehindu', 'vox', 'caravan', 'new_yorker','newyork_times','brain_pickings','tribune','tribune_ludhiana', 'bbc', 'the_wire']
+feed_tags = ['sci_am', 'thehindu', 'vox', 'empire_kop', 'caravan', 'new_yorker','newyork_times','brain_pickings','tribune','tribune_ludhiana', 'bbc', 'the_wire']
 # 'theguardian_world'
 local_feed_tags = []
 
 print("Yo, whaddup?\nWelcome to Pretty Parser - the slickest, bare-bones RSS feed aggregator, ever.\n")
 print("Do you want browse through my default feeds? \nWarning: Contains a Liverpool FC fan page and some other local newspapers.")
-stick2def = input("Press Enter for Yaas or 'N' for Nooo: ").lower()
+stick2def = raw_input("Press Enter for Yaas or 'N' for Nooo: ").lower()
 
 if stick2def == 'n':
     feeds = {}
     feed_tags = []
     print("\nAll right. You can add your own RSS links in the next step. Press Enter when you're done.\n")
     while True:
-        link = input("Input RSS URL: ")
+        link = raw_input("Input RSS URL: ")
         if link == '':
             break
-        tag = input("Input a name tag for this feed: ")
+        tag = raw_input("Input a name tag for this feed: ")
         feeds[tag] = link
 #        feed_tags.append(tag)
 
@@ -38,30 +49,34 @@ def fetch():
     if not os.path.exists(newpath):
         os.makedirs(newpath)
     os.chdir(newpath)
-    
+
     global feeds
     global feed_tags
 
     print("\nFetching data from sources.")
     for tag in feed_tags:
+        print(tag + "...")
         url_string = feeds[tag]
         file_name = str(tag+".xml")
         with open(file_name, 'wb') as f:
-            resp = requests.get(url_string, verify=False)
-            f.write(resp.content)
+#            resp = requests.get(url_string)
+            resp = http.request('GET',url_string)
+#            f.write(resp.content)
+            f.write(resp.data)
 ##        urr.urlretrieve(url_string, file_name)
-            
+    print("Done.")
+
 def populate():
     newpath = str(home_dir + '/XML_storage')
     if os.path.exists(newpath):
         os.chdir(newpath)
     else:
         raise OSError("XML data not found.")
-    
+
     global data
     global feeds
     global local_feed_tags
-    
+
     xml_files = os.listdir(".")
     for names in xml_files:
         if names.endswith(".xml"):
@@ -72,6 +87,7 @@ def populate():
         tag = name[:-4]
         print(tag)
         rss_file = open(name, encoding="utf8")
+#        rss_file = open(name, 'rb')
         soup = BS(rss_file, 'xml')
         local_data = []
         #parsing xml item-wise
@@ -108,9 +124,9 @@ def populate():
                 description = p.sub('',description)
                 if len(description) > 300:
                     description = description[:300] + " ..."
-            
-            if type(description) != str:
-                description = description.text
+
+#            if type(description) != str:
+#                description = description.text
             local_data.append({"title": title.text, "pubdate":pubdate.text,"author":author.text,"description":description,"link":links.text})
             if count > 10:
                 break
@@ -118,22 +134,26 @@ def populate():
         rss_file.close()
 
 def user_queries():
-    how_old = input("\nHow old would you like your news to be? (Input 0 for today's news): ")
+    how_old = raw_input("\nHow old would you like your news to be? (Input 0 for today's news): ")
     no_rep = True
-    if input("Do you want the articles to repeat after you've viewed them once? (Y/N) ").lower() == "Y":
-        no_rep = False 
-    num = input("How many articles do you want to see per page? Press Enter to use default (25): ")
+    if raw_input("Do you want the articles to repeat after you've viewed them once? (Y/N) ").lower() == "Y":
+        no_rep = False
+    num = raw_input("How many articles do you want to see per page? Press Enter to use default (25): ")
     if num == '':
         num = 25
     num = int(num)
     return how_old,num,no_rep
 
-def pparser(how_old,num,no_rep):
-    os.chdir(home_dir)
-    global data
-    global feeds
-    global local_feed_tags
-    html_file = open('tmp.html', 'w+', encoding="utf8")
+def create_article_html(article, counter, article_url):
+    '''
+    Takes an article object from newspaper and creates an html webpage out of it.
+    '''
+#    article.download()
+#    article.parse()
+#    text_paras = article.text.split('\n')
+    article_text = article.cleaned_text
+    text_paras = article_text.split("\n")
+
     html_content = '''
     <!DOCTYPE html>
     <html>
@@ -141,7 +161,7 @@ def pparser(how_old,num,no_rep):
         Pretty Parser
     </title>
     <head>
-            <meta charset="utf-8"> 
+            <meta charset="utf-8">
             <link href='https://fonts.googleapis.com/css?family=Bad+Script' rel='stylesheet' type='text/css'>
             <link href='https://fonts.googleapis.com/css?family=Economica:700' rel='stylesheet' type='text/css'>
             <link href='https://fonts.googleapis.com/css?family=PT+Serif' rel='stylesheet' type='text/css'>
@@ -150,6 +170,54 @@ def pparser(how_old,num,no_rep):
     <body>
         <div id='topbar'>
                 <h1><a href="https://github.com/gsidhu/Pretty_Parser">  Pretty Parser </a></h1>
+        </div>
+        <div class='item'>
+            <h2><a href='''+ str(article_url)+'''>'''+ article.title +'''</a></h2>
+            <p>'''
+
+    for i in range(len(text_paras)):
+        html_content += str("<p>" +text_paras[i]+"</p>")
+
+    html_content += '''
+    </div>
+    <hr>
+    </body>
+    <footer class="footer">
+        <h3><a href="https://gsidhu.github.io">  That Gurjot </a></h3>
+        <h4><a href="https://github.com/gsidhu/Pretty_Parser">  Source </a></h4>
+        <p> &copy; 2016. Some rights reserved. Built with &#9829; and lots of chocolate. </p>
+	</footer>
+    </body>
+    </html>
+    '''
+#    article_html_file = open(str('tmp_article_'+str(counter)+'.html'), 'w+')
+    article_html_file = open(str('tmp_article_'+str(counter)+'.html'), 'w+', encoding="utf8")
+    article_html_file.write(html_content)
+    article_html_file.close()
+
+def pparser(how_old,num,no_rep):
+    os.chdir(home_dir)
+    global data
+    global feeds
+    global local_feed_tags
+#    html_file = open('tmp.html', 'w+')
+    html_file = open('tmp.html', 'w+', encoding="utf8")
+    html_content = '''
+    <!DOCTYPE html>
+    <html>
+    <title>
+        Pretty Parser
+    </title>
+    <head>
+            <meta charset="utf-8">
+            <link href='https://fonts.googleapis.com/css?family=Bad+Script' rel='stylesheet' type='text/css'>
+            <link href='https://fonts.googleapis.com/css?family=Economica:700' rel='stylesheet' type='text/css'>
+            <link href='https://fonts.googleapis.com/css?family=PT+Serif' rel='stylesheet' type='text/css'>
+            <link rel="stylesheet" type="text/css" href="style.css" />
+    </head>
+    <body>
+        <div id='topbar'>
+            <h1><a href="https://github.com/gsidhu/Pretty_Parser">  Pretty Parser </a></h1>
         </div>
     '''
     counter = num
@@ -164,14 +232,21 @@ def pparser(how_old,num,no_rep):
             try:
                 article_date = datetime.datetime.strptime(data[tag_choice][article_choice]["pubdate"], '%a, %d %b %Y %H:%M:%S %z').date()
             except:
+                continue
                 article_date = datetime.datetime.strptime(data[tag_choice][article_choice]["pubdate"], '%a, %d %b %Y %H:%M:%S %Z').date()
+
+        article_url = data[tag_choice][article_choice]["link"]
+#        article = Article(article_url)
+        g = Goose()
+        article = g.extract(article_url)
+        create_article_html(article, counter, article_url)
 
         content = '''
             <div class='item'>
                     <span id = 'title'>
-                            <h2> ''' + str(str(num+1-counter)+". ") + ''' <a href= "''' + data[tag_choice][article_choice]["link"] + '''" target="_blank">''' + data[tag_choice][article_choice]["title"] + '''</a></h2>
+                            <h2> ''' + str(str(num+1-counter)+". ") + ''' <a href= "''' + str('tmp_article_'+str(counter)+'.html') + '''" target="_blank">''' + data[tag_choice][article_choice]["title"] + '''</a></h2>
                     </span>
-                    <span id ='pubdate'> 
+                    <span id ='pubdate'>
                             <h5>''' + data[tag_choice][article_choice]["pubdate"] + '''</h5>
                     </span>
                     <span id ='author'>
@@ -205,7 +280,7 @@ def pparser(how_old,num,no_rep):
 
     webbrowser.open('tmp.html')
 
-    again = input("More articles? Press Enter for Hell Yeah or any other key for Nuh-uh. : ").lower()
+    again = raw_input("More articles? Press Enter for Hell Yeah or any other key for Nuh-uh. : ").lower()
     if again == '':
         pparser(how_old,num,no_rep)
     elif again == 'n':
@@ -214,7 +289,7 @@ def pparser(how_old,num,no_rep):
 if __name__ == "__main__":
     newpath = str(home_dir + '/XML_storage')
     if os.path.exists(newpath):
-        if input("\nThe force senses some old XML data files.\nDo you want to generate a feed from this old data?\nEnter is yes, any other key is no: ") != '':
+        if raw_input("\nThe force senses some old XML data files.\nDo you want to generate a feed from this old data?\nEnter is yes, any other key is no: ") != '':
             fetch()
         populate()
         how_old,num,no_rep = user_queries()
